a962ee891de8c166cfa97363bfd2a72f7a86d543
andreojr <atipicode.ec@gmail.com>
2023-04-27 15:37:18 -0300

    up seed

--- prisma/data.ts
+++ prisma/data.ts
@@ -1,48 +1,145 @@
 export const users = [
-  {"id":"9e4f1742-4834-45b1-b8ac-59d82cf9b34c","nome":"André Luiz Oliveira Júnior","matricula":"223116037","curso":"engcomp"},
-  {"id":"53d8ff36-0bd6-40ef-ab2a-8ca086cf63e0","nome":"Mirella França","matricula":"223115221","curso":"engcomp"},
-  {"id":"c20bfa13-58d1-4a66-bdbd-df5b43147d12","nome":"Dimytri Gueudeville Tambone","matricula":"223116026","curso":"engcomp"},
-  {"id":"d12c2977-5880-48cc-8f2f-b24e4e17ad68","nome":"Júlio Ângelo dos Santos","matricula":"219120452","curso":"engcomp"},
-  {"id":"b211e083-702d-4382-b72a-233b7e1bcea9","nome":"Yasmin","matricula":"223115223","curso":"engcomp"},
-  {"id":"3e557c7e-78b1-4b63-a421-5be5b346a0ee","nome":"Felipe Daniel da Silveira Cruz França","matricula":"222215141","curso":"engcomp"},
-  {"id":"2ab51a2a-2e59-4511-b6d1-e75659c99895","nome":"João Kishimoto","matricula":"223116024","curso":"engcomp"},
-  {"id":"e20e11ea-32e6-47ce-9218-b863f6b9cfb0","nome":"Shênio Alves","matricula":"221120536","curso":"engcomp"},
-  {"id":"128127b6-2908-475a-86d1-2de3ddebf9e9","nome":"Emanuel Lucas","matricula":"223116048","curso":"engcomp"},
-  {"id":"f51e6cc7-d8b4-44cc-b02b-839ccf51b031","nome":"João Luis da Cruz de Souza","matricula":"223116032","curso":"engcomp"},
-  {"id":"ca17307c-0ca5-4a88-8974-dea6933be5b0","nome":"Paulo Nogueira","matricula":"220119501","curso":"engcomp"},
-  {"id":"50e85142-c477-4cc7-8733-39521ec0e55e","nome":"Mikelly","matricula":"221115979","curso":"engcomp"},
-  {"id":"f0c44241-c354-4196-94d7-d904a70e33b5","nome":"Moisés Santana","matricula":"222215130","curso":"engcomp"},
-  {"id":"032b3858-8200-4a4b-b598-ac3d4c62b93d","nome":"Jess","matricula":"222215129","curso":"engcomp"},
-  {"id":"c5cf9d00-31c5-4801-8877-3a7e27a44c0e","nome":"Matheus Brito","matricula":"222115963","curso":"engcomp"},
-  {"id":"7f5d8c49-6f72-4b6d-9fe9-b935083182b7","nome":"Laurinne","matricula":"217220658","curso":"engcomp"},
-  {"id":"f52fdb22-74d8-4a08-9a48-1dc7429a91ea","nome":"Fábio Miguel Mascarenhas dos Santos","matricula":"222118639","curso":"engcomp"},
-  {"id":"aaf9af3f-6c79-42af-9f99-d7fed7044722","nome":"Amanda Bastos de Melo Correia ","matricula":"223116033","curso":"engcomp"},
-  {"id":"98f14062-f215-45ef-b884-0279e3da3051","nome":"Júlia Teixeira Gonçalves ","matricula":"223116031","curso":"engcomp"}];
+  { "id": "9e4f1742-4834-45b1-b8ac-59d82cf9b34c", "nome": "André Luiz Oliveira Júnior", "matricula": "223116037", "curso": "engcomp" },
+  { "id": "53d8ff36-0bd6-40ef-ab2a-8ca086cf63e0", "nome": "Mirella França", "matricula": "223115221", "curso": "engcomp" },
+  { "id": "c20bfa13-58d1-4a66-bdbd-df5b43147d12", "nome": "Dimytri Gueudeville Tambone", "matricula": "223116026", "curso": "engcomp" },
+  { "id": "d12c2977-5880-48cc-8f2f-b24e4e17ad68", "nome": "Júlio Ângelo dos Santos", "matricula": "219120452", "curso": "engcomp" },
+  { "id": "b211e083-702d-4382-b72a-233b7e1bcea9", "nome": "Yasmin", "matricula": "223115223", "curso": "engcomp" },
+  { "id": "3e557c7e-78b1-4b63-a421-5be5b346a0ee", "nome": "Felipe Daniel da Silveira Cruz França", "matricula": "222215141", "curso": "engcomp" },
+  { "id": "2ab51a2a-2e59-4511-b6d1-e75659c99895", "nome": "João Kishimoto", "matricula": "223116024", "curso": "engcomp" },
+  { "id": "e20e11ea-32e6-47ce-9218-b863f6b9cfb0", "nome": "Shênio Alves", "matricula": "221120536", "curso": "engcomp" },
+  { "id": "128127b6-2908-475a-86d1-2de3ddebf9e9", "nome": "Emanuel Lucas", "matricula": "223116048", "curso": "engcomp" },
+  { "id": "f51e6cc7-d8b4-44cc-b02b-839ccf51b031", "nome": "João Luis da Cruz de Souza", "matricula": "223116032", "curso": "engcomp" },
+  { "id": "ca17307c-0ca5-4a88-8974-dea6933be5b0", "nome": "Paulo Nogueira", "matricula": "220119501", "curso": "engcomp" },
+  { "id": "50e85142-c477-4cc7-8733-39521ec0e55e", "nome": "Mikelly", "matricula": "221115979", "curso": "engcomp" },
+  { "id": "f0c44241-c354-4196-94d7-d904a70e33b5", "nome": "Moisés Santana", "matricula": "222215130", "curso": "engcomp" },
+  { "id": "032b3858-8200-4a4b-b598-ac3d4c62b93d", "nome": "Jess", "matricula": "222215129", "curso": "engcomp" },
+  { "id": "c5cf9d00-31c5-4801-8877-3a7e27a44c0e", "nome": "Matheus Brito", "matricula": "222115963", "curso": "engcomp" },
+  { "id": "7f5d8c49-6f72-4b6d-9fe9-b935083182b7", "nome": "Laurinne", "matricula": "217220658", "curso": "engcomp" },
+  { "id": "f52fdb22-74d8-4a08-9a48-1dc7429a91ea", "nome": "Fábio Miguel Mascarenhas dos Santos", "matricula": "222118639", "curso": "engcomp" },
+  { "id": "aaf9af3f-6c79-42af-9f99-d7fed7044722", "nome": "Amanda Bastos de Melo Correia ", "matricula": "223116033", "curso": "engcomp" },
+  { "id": "98f14062-f215-45ef-b884-0279e3da3051", "nome": "Júlia Teixeira Gonçalves ", "matricula": "223116031", "curso": "engcomp" }];
 
 export const requests = [
-  {"id":"bc483974-b00e-4f45-8dca-b9cd18f76c97","cor":"preta","tamanho":"G","user_id":"3e557c7e-78b1-4b63-a421-5be5b346a0ee","pay":false},
-  {"id":"542013d0-154e-4ab0-a1b8-f9d6eea6b59b","cor":"preta","tamanho":"P","user_id":"c20bfa13-58d1-4a66-bdbd-df5b43147d12","pay":false},
-  {"id":"7a612080-160f-49f2-8c11-b6c604d84ee5","cor":"vinho","tamanho":"G","user_id":"ca17307c-0ca5-4a88-8974-dea6933be5b0","pay":false},
-  {"id":"9ebcb87a-f25d-42f5-9b2a-54223b27446b","cor":"azul","tamanho":"G","user_id":"c5cf9d00-31c5-4801-8877-3a7e27a44c0e","pay":false},
-  {"id":"54dcf3b3-d258-4cee-a044-ee2c1483583e","cor":"preta","tamanho":"M","user_id":"f0c44241-c354-4196-94d7-d904a70e33b5","pay":false},
-  {"id":"3b2e164e-226f-4105-8078-2a28b6bafa67","cor":"preta","tamanho":"M","user_id":"d12c2977-5880-48cc-8f2f-b24e4e17ad68","pay":false},
-  {"id":"4ebc6d62-1005-47ef-9542-66309fb8113d","cor":"preta","tamanho":"M","user_id":"128127b6-2908-475a-86d1-2de3ddebf9e9","pay":false},
-  {"id":"b7952aa0-f5ff-483a-a3b2-227f7d85657d","cor":"preta","tamanho":"M","user_id":"f51e6cc7-d8b4-44cc-b02b-839ccf51b031","pay":false},
-  {"id":"14d5ec41-4144-4d86-8fdb-f2a358fd690c","cor":"preta","tamanho":"M","user_id":"e20e11ea-32e6-47ce-9218-b863f6b9cfb0","pay":false},
-  {"id":"34025169-fccd-4505-ade0-534f53ac4233","cor":"preta","tamanho":"G","user_id":"2ab51a2a-2e59-4511-b6d1-e75659c99895","pay":false},
-  {"id":"e36b0c79-0c65-4c85-896a-3d661c77b9c7","cor":"preta","tamanho":"G","user_id":"2ab51a2a-2e59-4511-b6d1-e75659c99895","pay":false},
-  {"id":"fd391b97-4e1d-4c1a-8e94-08d9a678fc1b","cor":"preta","tamanho":"G","user_id":"2ab51a2a-2e59-4511-b6d1-e75659c99895","pay":false},
-  {"id":"d879e663-9e8b-4c57-91bf-d76723de9415","cor":"preta","tamanho":"P","user_id":"50e85142-c477-4cc7-8733-39521ec0e55e","pay":false},
-  {"id":"ed12b5b5-564b-4f92-ab16-b6d082d86071","cor":"preta","tamanho":"G","user_id":"c5cf9d00-31c5-4801-8877-3a7e27a44c0e","pay":false},
-  {"id":"9027ea46-54c6-4bcb-a414-b4be651f100e","cor":"preta","tamanho":"G","user_id":"ca17307c-0ca5-4a88-8974-dea6933be5b0","pay":false},
-  {"id":"d7de2d36-72cf-42a9-b234-118f9cf85071","cor":"preta","tamanho":"M","user_id":"7f5d8c49-6f72-4b6d-9fe9-b935083182b7","pay":false},
-  {"id":"21e79a9b-59fd-4cf0-b4a6-ec1ea358b4e4","cor":"preta","tamanho":"M","user_id":"032b3858-8200-4a4b-b598-ac3d4c62b93d","pay":false},
-  {"id":"ac4856a3-dc0d-4c9f-a7a9-4afa1a68482a","cor":"branca","tamanho":"G","user_id":"f52fdb22-74d8-4a08-9a48-1dc7429a91ea","pay":false},
-  {"id":"b99520aa-ec61-42e2-bcaa-734e1f823e80","cor":"azul","tamanho":"G","user_id":"f52fdb22-74d8-4a08-9a48-1dc7429a91ea","pay":false},
-  {"id":"1036a5ea-acf8-4c96-a824-47b932dc4040","cor":"azul","tamanho":"M","user_id":"e20e11ea-32e6-47ce-9218-b863f6b9cfb0","pay":false},
-  {"id":"1ff041cf-fdb5-40b7-9ae5-cdefe8e5819b","cor":"preta","tamanho":"G","user_id":"b211e083-702d-4382-b72a-233b7e1bcea9","pay":false},
-  {"id":"f9cb7b60-83c0-4a39-89c9-24080262260b","cor":"azul","tamanho":"G","user_id":"9e4f1742-4834-45b1-b8ac-59d82cf9b34c","pay":false},
-  {"id":"0fd413e1-91fc-4d5f-a16a-e26ce1a7038f","cor":"preta","tamanho":"G","user_id":"9e4f1742-4834-45b1-b8ac-59d82cf9b34c","pay":false},
-  {"id":"77ab78dd-263a-4efa-a354-6491e472c3d7","cor":"preta","tamanho":"G","user_id":"f52fdb22-74d8-4a08-9a48-1dc7429a91ea","pay":false},
-  {"id":"652b5878-c5d2-461f-bb54-f6e27540639b","cor":"azul","tamanho":"GG","user_id":"53d8ff36-0bd6-40ef-ab2a-8ca086cf63e0","pay":false},
-  {"id":"5d39fba2-08ec-42ef-b616-f2824a775f17","cor":"azul","tamanho":"G","user_id":"aaf9af3f-6c79-42af-9f99-d7fed7044722","pay":false}];
+  { "id": "bc483974-b00e-4f45-8dca-b9cd18f76c97", "cor": "preta", "tamanho": "G", "user_id": "3e557c7e-78b1-4b63-a421-5be5b346a0ee", "pay": false },
+  { "id": "542013d0-154e-4ab0-a1b8-f9d6eea6b59b", "cor": "preta", "tamanho": "P", "user_id": "c20bfa13-58d1-4a66-bdbd-df5b43147d12", "pay": false },
+  { "id": "7a612080-160f-49f2-8c11-b6c604d84ee5", "cor": "vinho", "tamanho": "G", "user_id": "ca17307c-0ca5-4a88-8974-dea6933be5b0", "pay": false },
+  { "id": "9ebcb87a-f25d-42f5-9b2a-54223b27446b", "cor": "azul", "tamanho": "G", "user_id": "c5cf9d00-31c5-4801-8877-3a7e27a44c0e", "pay": false },
+  { "id": "54dcf3b3-d258-4cee-a044-ee2c1483583e", "cor": "preta", "tamanho": "M", "user_id": "f0c44241-c354-4196-94d7-d904a70e33b5", "pay": false },
+  { "id": "3b2e164e-226f-4105-8078-2a28b6bafa67", "cor": "preta", "tamanho": "M", "user_id": "d12c2977-5880-48cc-8f2f-b24e4e17ad68", "pay": false },
+  { "id": "4ebc6d62-1005-47ef-9542-66309fb8113d", "cor": "preta", "tamanho": "M", "user_id": "128127b6-2908-475a-86d1-2de3ddebf9e9", "pay": false },
+  { "id": "b7952aa0-f5ff-483a-a3b2-227f7d85657d", "cor": "preta", "tamanho": "M", "user_id": "f51e6cc7-d8b4-44cc-b02b-839ccf51b031", "pay": false },
+  { "id": "14d5ec41-4144-4d86-8fdb-f2a358fd690c", "cor": "preta", "tamanho": "M", "user_id": "e20e11ea-32e6-47ce-9218-b863f6b9cfb0", "pay": false },
+  {"id": "34025169-fccd-4505-ade0-534f53ac4233", "cor": "preta", "tamanho": "G", "user_id": "2ab51a2a-2e59-4511-b6d1-e75659c99895", "pay": false },
+  { "id": "e36b0c79-0c65-4c85-896a-3d661c77b9c7", "cor": "preta", "tamanho": "G", "user_id": "2ab51a2a-2e59-4511-b6d1-e75659c99895", "pay": false },
+  { "id": "fd391b97-4e1d-4c1a-8e94-08d9a678fc1b", "cor": "preta", "tamanho": "G", "user_id": "2ab51a2a-2e59-4511-b6d1-e75659c99895", "pay": false },
+  { "id": "d879e663-9e8b-4c57-91bf-d76723de9415", "cor": "preta", "tamanho": "P", "user_id": "50e85142-c477-4cc7-8733-39521ec0e55e", "pay": false },
+  { "id": "ed12b5b5-564b-4f92-ab16-b6d082d86071", "cor": "preta", "tamanho": "G", "user_id": "c5cf9d00-31c5-4801-8877-3a7e27a44c0e", "pay": false },
+  { "id": "9027ea46-54c6-4bcb-a414-b4be651f100e", "cor": "preta", "tamanho": "G", "user_id": "ca17307c-0ca5-4a88-8974-dea6933be5b0", "pay": false },
+  { "id": "d7de2d36-72cf-42a9-b234-118f9cf85071", "cor": "preta", "tamanho": "M", "user_id": "7f5d8c49-6f72-4b6d-9fe9-b935083182b7", "pay": false },
+  { "id": "21e79a9b-59fd-4cf0-b4a6-ec1ea358b4e4", "cor": "preta", "tamanho": "M", "user_id": "032b3858-8200-4a4b-b598-ac3d4c62b93d", "pay": false },
+  { "id": "ac4856a3-dc0d-4c9f-a7a9-4afa1a68482a", "cor": "branca", "tamanho": "G", "user_id": "f52fdb22-74d8-4a08-9a48-1dc7429a91ea", "pay": false },
+  { "id": "b99520aa-ec61-42e2-bcaa-734e1f823e80", "cor": "azul", "tamanho": "G", "user_id": "f52fdb22-74d8-4a08-9a48-1dc7429a91ea", "pay": false },
+  { "id": "1036a5ea-acf8-4c96-a824-47b932dc4040", "cor": "azul", "tamanho": "M", "user_id": "e20e11ea-32e6-47ce-9218-b863f6b9cfb0", "pay": false },
+  { "id": "1ff041cf-fdb5-40b7-9ae5-cdefe8e5819b", "cor": "preta", "tamanho": "G", "user_id": "b211e083-702d-4382-b72a-233b7e1bcea9", "pay": false },
+  { "id": "f9cb7b60-83c0-4a39-89c9-24080262260b", "cor": "azul", "tamanho": "G", "user_id": "9e4f1742-4834-45b1-b8ac-59d82cf9b34c", "pay": false },
+  { "id": "0fd413e1-91fc-4d5f-a16a-e26ce1a7038f", "cor": "preta", "tamanho": "G", "user_id": "9e4f1742-4834-45b1-b8ac-59d82cf9b34c", "pay": false },
+  { "id": "77ab78dd-263a-4efa-a354-6491e472c3d7", "cor": "preta", "tamanho": "G", "user_id": "f52fdb22-74d8-4a08-9a48-1dc7429a91ea", "pay": false },
+  { "id": "652b5878-c5d2-461f-bb54-f6e27540639b", "cor": "azul", "tamanho": "GG", "user_id": "53d8ff36-0bd6-40ef-ab2a-8ca086cf63e0", "pay": false },
+  { "id": "5d39fba2-08ec-42ef-b616-f2824a775f17", "cor": "azul", "tamanho": "G", "user_id": "aaf9af3f-6c79-42af-9f99-d7fed7044722", "pay": false },
+  {
+    "modelo": "classica",
+    "cor": "marinho",
+    "tamanho": "G",
+    "user_id": "c5cf9d00-31c5-4801-8877-3a7e27a44c0e"
+  },
+  {
+    "modelo": "classica",
+    "cor": "preta",
+    "tamanho": "M",
+    "user_id": "7f5d8c49-6f72-4b6d-9fe9-b935083182b7"
+  },
+  {
+    "modelo": "classica",
+    "cor": "branca",
+    "tamanho": "M",
+    "user_id": "7f5d8c49-6f72-4b6d-9fe9-b935083182b7"
+  },
+  {
+    "modelo": "classica",
+    "cor": "marinho",
+    "tamanho": "M",
+    "user_id": "7f5d8c49-6f72-4b6d-9fe9-b935083182b7"
+  },
+  {
+    "modelo": "classica",
+    "cor": "azul",
+    "tamanho": "G",
+    "user_id": "f52fdb22-74d8-4a08-9a48-1dc7429a91ea"
+  },
+  {
+    "modelo": "classica",
+    "cor": "preta",
+    "tamanho": "G",
+    "user_id": "b211e083-702d-4382-b72a-233b7e1bcea9"
+  },
+  {
+    "modelo": "classica",
+    "cor": "preta",
+    "tamanho": "G",
+    "user_id": "3e557c7e-78b1-4b63-a421-5be5b346a0ee"
+  },
+  {
+    "modelo": "classica",
+    "cor": "vermelha",
+    "tamanho": "G",
+    "user_id": "3e557c7e-78b1-4b63-a421-5be5b346a0ee"
+  },
+  {
+    "modelo": "classica",
+    "cor": "vermelha",
+    "tamanho": "P",
+    "user_id": "50e85142-c477-4cc7-8733-39521ec0e55e"
+  },
+  {
+    "modelo": "classica",
+    "cor": "preta",
+    "tamanho": "M",
+    "user_id": "f51e6cc7-d8b4-44cc-b02b-839ccf51b031"
+  },
+  {
+    "modelo": "classica",
+    "cor": "vinho",
+    "tamanho": "XG",
+    "user_id": "53d8ff36-0bd6-40ef-ab2a-8ca086cf63e0"
+  },
+  {
+    "modelo": "classica",
+    "cor": "preta",
+    "tamanho": "M",
+    "user_id": "9e4f1742-4834-45b1-b8ac-59d82cf9b34c"
+  },
+  {
+    "modelo": "classica",
+    "cor": "azul",
+    "tamanho": "G",
+    "user_id": "aaf9af3f-6c79-42af-9f99-d7fed7044722"
+  },
+  {
+    "modelo": "classica",
+    "cor": "vinho",
+    "tamanho": "G",
+    "user_id": "aaf9af3f-6c79-42af-9f99-d7fed7044722"
+  },
+  {
+    "modelo": "classica",
+    "cor": "cinza",
+    "tamanho": "M",
+    "user_id": "e20e11ea-32e6-47ce-9218-b863f6b9cfb0"
+  },
+  {
+    "modelo": "classica",
+    "cor": "preta",
+    "tamanho": "P",
+    "user_id": "c20bfa13-58d1-4a66-bdbd-df5b43147d12"
+  }
+];
