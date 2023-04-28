1eaa64590cb5fb54bd16c2359cbe7c27930bdde4
andreojr <atipicode.ec@gmail.com>
2023-04-28 16:07:40 -0300

    up

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,62 @@ import { User } from "@prisma/client";
 
 export async function routes(server: FastifyInstance) {
 
-    server.get("/users/pay/:matricula", async (request, reply) => {
+    server.get("/etapa-atual", (request, reply) => {
+        reply.status(200).send(process.env.ETAPA_ATUAL);
+    });
+
+    server.get("/users", async (request, reply) => {
+        const users = await db.user.findMany();
+
+        const usersWithPay: Array<User> = [];
+        
+        await Promise.all(
+            users.map(async (user, i) => {
+                let userWithPay = {...user, pay: true};
+                const requests = await db.request.findMany({
+                    where: { user_id: user.id }
+                });
+                requests.forEach(async request => {
+                    if (!request.pay) userWithPay.pay = false;
+                });
+                usersWithPay.push(userWithPay);
+            })
+        );
+        
+        reply.status(200).send(usersWithPay);
+    });
+
+    server.patch("/users/unpay/:matricula", async (request, reply) => {
+
+        const searchUserSchema = z.object({
+            matricula: z.string().regex(/^\d+$/),
+        });
+        
+        const { matricula } = searchUserSchema.parse(request.params);
+
+        const user = await db.user.findUnique({
+            where: { matricula }
+        });
+
+        if (user) {
+            const requests = await db.request.findMany({
+                where: { user_id: user.id }
+            });
+
+            requests.forEach(async request => {
+                await db.request.update({
+                    where: { id: request.id },
+                    data: { pay: false }
+                });
+            });
+
+            reply.status(200).send();
+        } else {
+            reply.status(404).send();
+        }
+    });
+    
+    server.patch("/users/pay/:matricula", async (request, reply) => {
 
         const searchUserSchema = z.object({
             matricula: z.string().regex(/^\d+$/),
@@ -37,45 +92,47 @@ export async function routes(server: FastifyInstance) {
 
     server.post("/requests", async (request, reply) => {
 
-
-        const createShirtsSchema = z.object({
-            cor: z.string(),
-            tamanho: z.string().min(1).max(2),
-            modelo: z.enum(["classica", "alternativa"]),
-        });
-
-        const createRequestSchema = z.object({
-            nome: z.string(),
-            matricula: z.string().regex(/^\d+$/),
-            curso: z.string(),
-            shirts: z.array(createShirtsSchema),
-        });
-
-        const { nome, matricula, curso, shirts } = createRequestSchema.parse(request.body);
-
-        const userExists = await db.user.findUnique({ where: { matricula } });
-        let user: User;
-        if (!userExists) {
-            user = await db.user.create({
-                data: {
-                    nome,
-                    matricula,
-                    curso,
-                }
+        if (Number(process.env.ETAPA_ATUAL) === 1) {
+            const createShirtsSchema = z.object({
+                cor: z.string(),
+                tamanho: z.string().min(1).max(2),
+                modelo: z.enum(["classica", "alternativa"]),
             });
-        } else {
-            user = userExists;
-        }
-        
-        shirts.forEach(async shirt => {
-            await db.request.create({
-                data: {
-                    ...shirt,
-                    user_id: user.id,
-                }
-            })
-        });
-        reply.status(201).send();
+    
+            const createRequestSchema = z.object({
+                nome: z.string(),
+                matricula: z.string().regex(/^\d+$/),
+                curso: z.string(),
+                shirts: z.array(createShirtsSchema),
+            });
+    
+            const { nome, matricula, curso, shirts } = createRequestSchema.parse(request.body);
+    
+            const userExists = await db.user.findUnique({ where: { matricula } });
+            let user: User;
+            if (!userExists) {
+                user = await db.user.create({
+                    data: {
+                        nome,
+                        matricula,
+                        curso,
+                    }
+                });
+            } else {
+                user = userExists;
+            }
+            
+            shirts.forEach(async shirt => {
+                await db.request.create({
+                    data: {
+                        ...shirt,
+                        user_id: user.id,
+                    }
+                })
+            });
+            reply.status(201).send();
+        } else reply.status(500).send();
+
     });
 
     server.get("/login/:matricula", async (request, reply) => {
@@ -130,12 +187,23 @@ export async function routes(server: FastifyInstance) {
 
     server.delete("/requests/:id", async (request, reply) => {
 
+        if (Number(process.env.ETAPA_ATUAL) === 1) {
+            const getRequestsById = z.object({
+                id: z.string().uuid(),
+            });
+    
+            const { id } = getRequestsById.parse(request.params);
+            await db.request.delete({ where: { id } })
+            reply.status(200).send();
+        } else reply.status(500).send();
+    });
+
+    server.get("/paypass-verify/:pass", async (request, reply) => {
         const getRequestsById = z.object({
-            id: z.string().uuid(),
+            pass: z.enum([String(process.env.PAYPASS)]),
         });
 
-        const { id } = getRequestsById.parse(request.params);
-        await db.request.delete({ where: { id } })
+        const { pass } = getRequestsById.parse(request.params);
         reply.status(200).send();
     });
 }
